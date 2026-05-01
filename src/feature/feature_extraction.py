88ff8f2724fe2e7a2f4ac7f3dabb7c296b3124a7
Dyu20705 <duy28092005yh@gmail.com>
2026-05-01 15:32:55 +0700

    Implement vocabulary management with word addition, indexing, and corpus building

--- src/feature/feature_extraction.py
+++ src/feature/feature_extraction.py
@@ -1,3 +1,16 @@
 #convert text → vector
-# class CountVectorizer:
-#     def transform(self, tokens) -> vector
+class CountVectorizer:
+    def fit(self, documents):
+        pass
+            
+    def transform(self, documents):
+        pass
+    def create_sparse_matrix(self, vectors):
+        pass
+class TfidfVectorizer:
+    def fit(self, documents):
+        pass
+    def transform(self, documents):
+        pass
+    def create_sparse_matrix(self, vectors):
+        pass
